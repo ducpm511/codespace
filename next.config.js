1ee69fd2a9248b73c8b0001190046f3c7ae55b16
ducpm511 <duc@25holdings.com>
2023-03-20 21:11:59 +0700

    - init project

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reactStrictMode: false,
+  optimizeFonts: false,
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  }
+}
